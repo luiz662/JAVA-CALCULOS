e1af8e637b69c55730031a30aa204f7944cb48e6
luiz662 <141882626+luiz662@users.noreply.github.com>
2024-11-05 21:15:47 -0300

    Add files via upload

---
+++ ex027 copy/js/java.js
@@ -0,0 +1,5 @@
+var idade = Number(prompt ("digite sua idade"))
+var verificacaoIdade = idade >= 18 ? "maior de idade " : "menor de idade"
+alert(verificacaoIdade)
+
+
---
+++ ex028/js/java.js
@@ -0,0 +1,5 @@
+function compararNumeros(num1, num2){
+return num1 == num2 ? "São iguais" : "Não são iguais";
+}
+
+alert(compararNumeros(5,6)); 
